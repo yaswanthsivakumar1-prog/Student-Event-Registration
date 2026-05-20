a3049958fa56a9ee11c6c114c221d15ec66e1777
yaswanth <yaswanthsivakumar1@gmail.com>
2026-05-21 00:52:37 +0530

    connecting..the both ends

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const rawApiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = rawApiUrl.replace(/\/$/, '') + '/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
